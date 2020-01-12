46bf327fd368b3838b545220d55fe63cdee0baac
George Ellickson <GEllickson@users.noreply.github.com>
2020-01-12 13:25:34 -0800

    SingleLiveEvent for dispatching one-time events

---
+++ app/src/main/java/com/georgeellickson/giphyviewer/util/SingleLiveEvent.kt
@@ -0,0 +1,38 @@
+package com.georgeellickson.giphyviewer.util
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * Lifecycle aware observer for cases where we only need a one-time event (i.e. not every time a
+ * lifecycle becomes active). For example, navigation events, toast messages, etc.
+ *
+ * Modified from Google sample:
+ * https://github.com/android/architecture-samples/blob/dev-todo-mvvm-live/todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/SingleLiveEvent.java
+ */
+class SingleLiveEvent<T> : MutableLiveData<T>() {
+
+    private val pending: AtomicBoolean = AtomicBoolean(false)
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner, Observer<T> { t ->
+            if (pending.compareAndSet(true, false)) {
+                observer.onChanged(t)
+            }
+        })
+    }
+
+    override fun setValue(t: T?) {
+        pending.set(true)
+        super.setValue(t)
+    }
+
+    /**
+     * For when T is Void. I.e. we just what the callback without a value
+     */
+    fun callOnChanged() {
+        value = null
+    }
+}
