2a81efa79f07a1bee349163de15dc4c1e13491ad
George Ellickson <GEllickson@users.noreply.github.com>
2020-01-12 10:20:59 -0800

    GiphyKeyPref for storing Giphy API key

--- app/src/main/java/com/georgeellickson/giphyviewer/MainActivity.kt
+++ app/src/main/java/com/georgeellickson/giphyviewer/MainActivity.kt
@@ -2,11 +2,17 @@ package com.georgeellickson.giphyviewer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.georgeellickson.giphyviewer.storage.GiphyKeyPref
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var prefs: GiphyKeyPref
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        application.appComponent.inject(this)
     }
 }
---
+++ app/src/main/java/com/georgeellickson/giphyviewer/storage/GiphyKeyPref.kt
@@ -0,0 +1,21 @@
+package com.georgeellickson.giphyviewer.storage
+
+import android.content.Context
+import android.content.SharedPreferences
+import javax.inject.Inject
+
+private const val PREF_FILE = "giphy_keys"
+private const val KEY_API = "giphy_api_key"
+
+class GiphyKeyPref @Inject constructor(context: Context) {
+
+    private val prefs: SharedPreferences = context.getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE)
+
+    fun setApiKey(apiKey: String) = prefs.edit().putString(KEY_API, apiKey).apply()
+
+
+    fun getApiKey(): String = prefs.getString(KEY_API, "") ?: ""
+
+    fun hasApiKey(): Boolean = getApiKey().isNotEmpty()
+
+}
