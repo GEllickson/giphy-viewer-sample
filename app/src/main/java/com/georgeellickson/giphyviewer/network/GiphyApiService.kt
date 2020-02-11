26db03a2a289bb70f353e8be7a345c033e211626
George Ellickson <GEllickson@users.noreply.github.com>
2020-02-10 22:18:02 -0800

    Fix more gifs to screen. Load more total.

--- app/src/main/java/com/georgeellickson/giphyviewer/network/GiphyApiService.kt
+++ app/src/main/java/com/georgeellickson/giphyviewer/network/GiphyApiService.kt
@@ -8,6 +8,6 @@ interface GiphyApiService {
     @GET("gifs/trending")
     suspend fun getTrending(
         @Query("api_key") api_key: String,
-        @Query("limit") limit: Int = 20): GiphyTrendingHolder
+        @Query("limit") limit: Int = 40): GiphyTrendingHolder
 
 }
