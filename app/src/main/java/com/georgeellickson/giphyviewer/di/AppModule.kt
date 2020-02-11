9b826b7e8bbb0d8c09868569ea0967e4a965da9c
George Ellickson <GEllickson@users.noreply.github.com>
2020-02-11 09:34:02 -0800

    Add okhttp3 logging-interceptor dependency

--- app/src/main/java/com/georgeellickson/giphyviewer/di/AppModule.kt
+++ app/src/main/java/com/georgeellickson/giphyviewer/di/AppModule.kt
@@ -4,6 +4,8 @@ import com.georgeellickson.giphyviewer.network.GiphyApiService
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -12,10 +14,15 @@ class AppModule {
 
     @Provides
     fun giphyApiService(): GiphyApiService {
+        val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
         val moshi = Moshi.Builder().build()
         return Retrofit.Builder()
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(client)
             .baseUrl("https://api.giphy.com/v1/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(GiphyApiService::class.java)
     }
